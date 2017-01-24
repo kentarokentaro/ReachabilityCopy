2b2e87771168308028a2754fcbf5257da4110baa
Kentaro Miura <z.kentaro.miura@g-pc-4072.intra.gree-office.net>
2017-01-24 16:36:32 +0900

    modify Reachability header

--- ReachabilityCopy/ReachabilityCopy.h
+++ ReachabilityCopy/ReachabilityCopy.h
@@ -7,7 +7,40 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <netinet/in.h>
+
+
+// connect type
+typedef enum : NSUInteger {
+    NotReachable = 0,
+    ReachableViaWiFi,
+    ReachableViaWWAN,
+} NetworkStatus;
+
+#pragma mark IPv6 Support
+
+extern NSString *kReachabilityChangedNotification;
 
 @interface ReachabilityCopy : NSObject
+
+// ホストネームを取得する
++ (instancetype)reachabilityWithHostName:(NSString*)hostName;
+
+// IPアドレスを取得する
++ (instancetype)reachabilityWithAddress:(const struct sockaddr *)hostAddress;
+
+// デフォルトルートの利用可能をチェックする
++ (instancetype)reachabilityForInternetConnection;
+
+#pragma mark reachabilityForLocalWiFI
+
+- (BOOL)startNotifier;
+- (void)stopNotifier;
+
+// ステータスを返す（enumで設定した値）
+- (NetworkStatus)currentRechabilityStatus;
+
+- (BOOL)connectionRequiered;
 
 @end
