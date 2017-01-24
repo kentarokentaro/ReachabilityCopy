989481d83032ded3d835c01302968a6610402e61
Kentaro Miura <z.kentaro.miura@g-pc-4072.intra.gree-office.net>
2017-01-24 14:32:50 +0900

    Reachability file add

---
+++ ReachabilityCopy/ReachabilityCopy.h
@@ -0,0 +1,13 @@
+//
+//  ReachabilityCopy.h
+//  ReachabilityCopy
+//
+//  Created by Kentaro Miura on 2017/01/24.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReachabilityCopy : NSObject
+
+@end
---
+++ ReachabilityCopy/ReachabilityCopy.m
@@ -0,0 +1,13 @@
+//
+//  ReachabilityCopy.m
+//  ReachabilityCopy
+//
+//  Created by Kentaro Miura on 2017/01/24.
+//
+//
+
+#import "ReachabilityCopy.h"
+
+@implementation ReachabilityCopy
+
+@end
