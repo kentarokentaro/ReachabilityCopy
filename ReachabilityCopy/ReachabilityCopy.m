b7f8b6d0023292cb1cafe3b0cecc35f8223d523b
Kentaro Miura <z.kentaro.miura@g-pc-4072.intra.gree-office.net>
2017-01-24 16:44:44 +0900

    Modify Reachability.m. Add grobal method declared with header

--- ReachabilityCopy/ReachabilityCopy.m
+++ ReachabilityCopy/ReachabilityCopy.m
@@ -10,4 +10,48 @@
 
 @implementation ReachabilityCopy
 
+// ホストネームを取得する
++ (instancetype)reachabilityWithHostName:(NSString*)hostName
+{
+    ReachabilityCopy* returnValue = NULL;
+    return returnValue;
+}
+
+// IPアドレスを取得する
++ (instancetype)reachabilityWithAddress:(const struct sockaddr *)hostAddress
+{
+    ReachabilityCopy* returnValue = NULL;
+    return returnValue;
+}
+
+// デフォルトルートの利用可能をチェックする
++ (instancetype)reachabilityForInternetConnection
+{
+    return NULL;
+}
+
+#pragma mark reachabilityForLocalWiFI
+
+- (BOOL)startNotifier
+{
+    BOOL returnValue = NO;
+    return returnValue;
+}
+
+- (void)stopNotifier
+{
+}
+
+// ステータスを返す（enumで設定した値）
+- (NetworkStatus)currentRechabilityStatus
+{
+    NetworkStatus returnValue = NotReachable;
+    return returnValue;
+}
+
+- (BOOL)connectionRequiered
+{
+    return NO;
+}
+
 @end
