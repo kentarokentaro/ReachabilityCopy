736774a3b81c98c0c115b5d10ef9fc0e5f922ece
kentaroMiura <oikimutida@yahoo.co.jp>
2017-01-30 02:01:36 +0900

    modify ViewController. WIP

--- ReachabilityCopy/ViewController.m
+++ ReachabilityCopy/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ReachabilityCopy.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *summaryLabel;
@@ -19,6 +20,8 @@
 @property (weak, nonatomic) IBOutlet UIImageView *internetConnectionImageView;
 @property (weak, nonatomic) IBOutlet UITextField *internetConnectionTextFieldView;
 
+@property (nonatomic) ReachabilityCopy *hostReachability;
+@property (nonatomic) ReachabilityCopy *internetReachability;
 
 @end
 
@@ -26,10 +29,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //通信状況が変わる際に、都度通知される
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reachabilityChanged:) name:kReachabilityChangedNotification object:nil];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+/*!
+ * Called by Reachability whenever status changes.
+ */
+- (void)reachabilityChanged:(NSNotification *)note
+{
+    ReachabilityCopy* curReach = [note object];
+    NSParameterAssert([curReach isKindOfClass:[ReachabilityCopy class]]);
+    [self updateInterfaceWithReachability:curReach];
+}
+
+- (void)updateInterfaceWithReachability:(ReachabilityCopy *)reachability
+{
+    if(reachability == self.hostReachability)
+    {
+        
+    }
+    
+}
+
+- (void)configureTextField:(UITextField *)textField imageView:(UIImageView *)imageView reachabililty:(ReachabilityCopy *)reachability
+{
+
+}
+
+
+//
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:kReachabilityChangedNotification object:nil];
 }
 
 @end
